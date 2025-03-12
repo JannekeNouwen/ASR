e94d20e4d0244a57f815ced446a9ff5736280d9e
JannekeNouwen <janneke@hoefnix.com>
2025-03-12 17:12:13 +0100

    Remove commented-out code and clean up setup script for clarity

--- emotion_recognition/emotion_recognition_inference.py
+++ emotion_recognition/emotion_recognition_inference.py
@@ -1,11 +1,14 @@
-import pandas as pd
-from speechbrain.inference.interfaces import foreign_class
+# THIS IS OLD, DO NOT USE
 
-from emotion_recognition_wav2vec import custom_interface
-from emotion_recognition_wav2vec.custom_interface import CustomEncoderWav2vec2Classifier
-from tqdm import tqdm
-input_path = "/home/janneke/scripts/personal/ASR-stuff/data/emotions_dataset/Crema/1001_DFA_ANG_XX.wav"
-source = custom_interface.__file__
+
+# import pandas as pd
+# from speechbrain.inference.interfaces import foreign_class
+
+# from emotion_recognition_wav2vec import custom_interface
+# from emotion_recognition_wav2vec.custom_interface import CustomEncoderWav2vec2Classifier
+# from tqdm import tqdm
+# input_path = "/home/janneke/scripts/personal/ASR-stuff/data/emotions_dataset/Crema/1001_DFA_ANG_XX.wav"
+# source = custom_interface.__file__
 
 # emotions in the CREMA-D dataset
 
@@ -13,37 +16,37 @@ source = custom_interface.__file__
 # anger, happiness, excitement, sadness, frustration, fear, surprise, other and neutral state
 
 
-def main():
-    crema_data = pd.read_csv("./data/crema_d/crema.csv")
-    print(crema_data.head())
-    classifier = load_model()
+# def main():
+#     crema_data = pd.read_csv("./data/crema_d/crema.csv")
+#     print(crema_data.head())
+#     classifier = load_model()
 
-    # Create a subset for testing
-    crema_data = crema_data.sample(20)
+#     # Create a subset for testing
+#     crema_data = crema_data.sample(20)
 
-    predictions = []
-    for i, row in tqdm(crema_data.iterrows(), total=len(crema_data)):
-        emotion = row["emotion"]
-        input_path = f"./data/crema_d/audiofiles/{row['speaker']}_{row['sentence']}_{emotion.upper()}_{row['emotion_level']}.wav"
-        predictions.append(predict(classifier, input_path))
+#     predictions = []
+#     for i, row in tqdm(crema_data.iterrows(), total=len(crema_data)):
+#         emotion = row["emotion"]
+#         input_path = f"./data/crema_d/audiofiles/{row['speaker']}_{row['sentence']}_{emotion.upper()}_{row['emotion_level']}.wav"
+#         predictions.append(predict(classifier, input_path))
 
-    crema_data["predicted_emotion"] = predictions
-    crema_data.to_csv("./data/crema_d/crema_predictions.csv")
-
-
-def load_model():
-    classifier: CustomEncoderWav2vec2Classifier = foreign_class(
-        source=source.replace("/custom_interface.py", ""),
-        pymodule_file="custom_interface.py",
-        classname="CustomEncoderWav2vec2Classifier",
-    )
-    return classifier
+#     crema_data["predicted_emotion"] = predictions
+#     crema_data.to_csv("./data/crema_d/crema_predictions.csv")
 
 
-def predict(classifier: CustomEncoderWav2vec2Classifier, input_path):
-    out_prob, score, index, text_lab = classifier.classify_file(input_path)
-    return text_lab
+# def load_model():
+#     classifier: CustomEncoderWav2vec2Classifier = foreign_class(
+#         source=source.replace("/custom_interface.py", ""),
+#         pymodule_file="custom_interface.py",
+#         classname="CustomEncoderWav2vec2Classifier",
+#     )
+#     return classifier
 
 
-if __name__ == "__main__":
-    main()
+# def predict(classifier: CustomEncoderWav2vec2Classifier, input_path):
+#     out_prob, score, index, text_lab = classifier.classify_file(input_path)
+#     return text_lab
+
+
+# if __name__ == "__main__":
+#     main()
--- emotion_recognition/setup_emotion_recognition.sh
+++ emotion_recognition/setup_emotion_recognition.sh
@@ -20,20 +20,22 @@ else
     exit 1
 fi
 
-# Download IEMOCAP dataset
-wget https://huggingface.co/datasets/Ar4ikov/iemocap_audio_text/blob/main/data/train-00000-of-00003-9213b91aae6dc76d.parquet
+# # Download IEMOCAP dataset
+# wget https://huggingface.co/datasets/Ar4ikov/iemocap_audio_text/blob/main/data/train-00000-of-00003-9213b91aae6dc76d.parquet
 
 
-# Install speechbrain
-pip install git+https://github.com/speechbrain/speechbrain.git@develop
+# # Install speechbrain
+# pip install git+https://github.com/speechbrain/speechbrain.git@develop
 
-# Clone the emotion recognition repository
-git clone git@hf.co:speechbrain/emotion-recognition-wav2vec2-IEMOCAP
-mv emotion-recognition-wav2vec2-IEMOCAP emotion_recognition_wav2vec
-cd emotion_recognition_wav2vec
-wget https://huggingface.co/speechbrain/emotion-recognition-wav2vec2-IEMOCAP/resolve/main/model.ckpt
-wget https://huggingface.co/speechbrain/emotion-recognition-wav2vec2-IEMOCAP/blob/main/wav2vec2.ckpt
-export PYTHONPATH=$PYTHONPATH:$(pwd)/emotion_recognition
+# # Clone the emotion recognition repository
+# git clone git@hf.co:speechbrain/emotion-recognition-wav2vec2-IEMOCAP
+# mv emotion-recognition-wav2vec2-IEMOCAP emotion_recognition_wav2vec
+# cd emotion_recognition_wav2vec
+# wget https://huggingface.co/speechbrain/emotion-recognition-wav2vec2-IEMOCAP/resolve/main/model.ckpt
+# wget https://huggingface.co/speechbrain/emotion-recognition-wav2vec2-IEMOCAP/blob/main/wav2vec2.ckpt
+# export PYTHONPATH=$PYTHONPATH:$(pwd)/emotion_recognition
 
-p=$(pwd)
-printf 'Please copy and paste the following line to the file ~/.bashrc (nano ~/.bashrc): \n export PYTHONPATH="$PYTHONPATH:%s/emotion_recognition_wav2vec"\n' "$p"
+# p=$(pwd)
+# printf 'Please copy and paste the following line to the file ~/.bashrc (nano ~/.bashrc): \n export PYTHONPATH="$PYTHONPATH:%s/emotion_recognition_wav2vec"\n' "$p"
+
+echo "Setup complete!"
