cd1d6dcdf93c58cd5cfc2ffcf719b4dc50c178d1
JannekeNouwen <janneke@hoefnix.com>
2025-03-12 17:11:55 +0100

    add W&B integration, improve dataset handling, and implement metrics computation

--- emotion_recognition/emotion_recognition_training.py
+++ emotion_recognition/emotion_recognition_training.py
@@ -1,20 +1,28 @@
+import os
 import random
 
+import evaluate
 import numpy as np
 import pandas as pd
+import wandb
 from datasets import Audio, Dataset, DatasetDict, load_dataset
 from transformers import AutoFeatureExtractor, AutoModelForAudioClassification, Trainer, TrainingArguments
-import os
+
 # Set random state
 random.seed(42)
 
 
 def main():
-    os.makedirs("./models/emotion_recognition", exist_ok=True)
-    
+    # I hope that you only need to change these 3 variables to run this script
+    dataset_name = "crema_d"
+    task = "emotion_recognition"
+    use_wandb = True
+
+    os.makedirs(f"./models/{task}", exist_ok=True)
+
     metadata, dataset = load_data(
-        "./data/crema_d/audiofiles",
-        metadata_csv_path="./data/cremad/crema.csv",
+        f"./data/{dataset_name}/audiofiles",
+        metadata_csv_path=f"./data/{dataset_name}/metadata.csv",
         label_column_name="emotion_id",
         speaker_column_name="speaker",
     )
@@ -25,20 +33,23 @@ def main():
     print(f"Number of training examples: {len(dataset['train'])}")
     print(f"Number of testing examples: {len(dataset['test'])}")
 
-    dataset.save_to_disk("./data/crema_d/cremad.hf")
+    dataset.save_to_disk(f"./data/{dataset_name}/{dataset_name}.hf")
 
     encoded_dataset, feature_extractor = preprocess_dataset(dataset)
 
     model = load_model(num_labels=len(metadata["emotion_id"].unique()))
 
-    trainer = get_trainer(model, encoded_dataset, feature_extractor)
+    trainer = get_trainer(model, encoded_dataset, feature_extractor, use_wandb=use_wandb)
 
-    output = trainer.evaluate()
+    output = trainer.evaluate(eval_dataset=encoded_dataset["test"])
     print(output)
 
-    trainer.save_model("./models/emotion_recognition")
+    trainer.train()
 
-    # trainer.train()
+    output = trainer.evaluate(eval_dataset=encoded_dataset["test"])
+    print(output)
+
+    trainer.save_model(f"./models/{task}")
 
 
 def load_data(
@@ -66,12 +77,14 @@ def make_train_test_split(dataset: Dataset, unique_speakers: np.ndarray, split_r
     test_set = dataset.filter(lambda example: example["speaker"] in test_speakers)
 
     # Verify no overlap
+    dataset = DatasetDict({"train": train_set, "test": test_set})
+
     assert len(set(dataset["train"]["speaker"]).intersection(set(dataset["test"]["speaker"]))) == 0, (
         "Speaker overlap detected between train and test!"
     )
 
     # Combine into DatasetDict
-    return DatasetDict({"train": train_set, "test": test_set})
+    return dataset
 
 
 def preprocess_dataset(dataset: DatasetDict) -> tuple[DatasetDict, AutoFeatureExtractor]:
@@ -93,13 +106,34 @@ def load_model(num_labels: int) -> AutoModelForAudioClassification:
     return AutoModelForAudioClassification.from_pretrained("facebook/wav2vec2-base", num_labels=num_labels)
 
 
-def get_trainer(model: AutoModelForAudioClassification, encoded_dataset: DatasetDict, feature_extractor: AutoFeatureExtractor) -> Trainer:
+def compute_metrics(eval_preds):
+    metric = evaluate.load("accuracy")
+    logits, labels = eval_preds
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+
+def get_trainer(
+    model: AutoModelForAudioClassification, encoded_dataset: DatasetDict, feature_extractor: AutoFeatureExtractor, use_wandb: bool
+) -> Trainer:
+    if use_wandb:
+        wandb.login()
+        os.environ["WANDB_PROJECT"] = "ASR2025"  # name your W&B project
+        os.environ["WANDB_LOG_MODEL"] = "checkpoint"  # log all model checkpoints
+
+    batch_size = 16
+    epochs = 5
     training_args = TrainingArguments(
         output_dir="./results",
         evaluation_strategy="epoch",
         save_strategy="epoch",
         learning_rate=3e-5,
-        num_train_epochs=5,
+        num_train_epochs=epochs,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=8,
+        report_to="wandb" if use_wandb else "none",
+        logging_strategy="steps",
+        logging_steps=5890 * epochs / batch_size / 20
     )
 
     return Trainer(
@@ -107,5 +141,10 @@ def get_trainer(model: AutoModelForAudioClassification, encoded_dataset: Dataset
         args=training_args,
         train_dataset=encoded_dataset["train"],
         eval_dataset=encoded_dataset["test"],
+        compute_metrics=compute_metrics,
         tokenizer=feature_extractor,
     )
+
+
+if __name__ == "__main__":
+    main()
